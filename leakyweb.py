b696f312f7406c7cf3b98efa1850292529016529
mudou <764191928@qq.com>
2019-09-27 21:09:26 +0800

    做了一点点

--- leakyweb.py
+++ leakyweb.py
@@ -1,8 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for,flash
+from flask.ext.login import login_user
+from . import auth
+from ..models import User
+from .forms import LoginForm
 
-app=Flask(__name__)
-@app.route('/')
+@app.route('/login',methods=['GET','POST'])
 def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not 
     return render_template('login.html')
 
 @app.route('/info')   
